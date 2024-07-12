f98c96e7a62017b5e9a727ba6bd0972d8e61d32d
humfrey-atg <humfreybrandes@auctiontechnologygroup.com>
2024-07-12 20:38:46 +0100

    Add selling out section

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const colors = ['yellow', 'pink', 'red', 'green', 'magenta', 'orange', 'cyan', 'blue'];
-    const showItems = document.querySelectorAll('.show-item');
+    const showItems = document.querySelectorAll('.sold-out-shows .show-item');
 
     showItems.forEach((item, index) => {
         const color = colors[index % colors.length];
         item.style.color = color;
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('a');
+    links.forEach(function(link) {
+        link.setAttribute('target', '_blank');
     });
 });
